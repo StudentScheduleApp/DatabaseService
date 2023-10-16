0c4d6b7363aa9aa754af129ab8892e9aec4c5d92
IlyaKrn <ilyakornoukhov@gmail.com>
2023-10-16 20:11:32 +0300

    bugfix

--- src/main/java/com/studentscheduleapp/databaseservice/api/GroupController.java
+++ src/main/java/com/studentscheduleapp/databaseservice/api/GroupController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("api/group")
+@RequestMapping("api/groups")
 public class GroupController {
 
     @Autowired
--- src/main/java/com/studentscheduleapp/databaseservice/api/ScheduleTemplateController.java
+++ src/main/java/com/studentscheduleapp/databaseservice/api/ScheduleTemplateController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/scheduleTemplate")
+@RequestMapping("api/scheduleTemplates")
 public class ScheduleTemplateController {
 
     @Autowired
